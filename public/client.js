95e2701460210c09c175fb2c9d1c96762af4ed5a
retro <mossiat.jeoffrey@outlook.com>
2019-11-22 09:40:46 +0100

    ghost commit

--- public/client.js
+++ public/client.js
@@ -3,12 +3,70 @@ var socket = io();
 var el = document.getElementById('server-time');
 
 
+// Display server time
 socket.on('time', function(timeString) {
     el.innerHTML = 'Server time: ' + timeString;
 });
 
+
+
 socket.on('hello', function(message){
-  document.getElementById('serverMessages').textContent = "" + message.content;
-  document.getElementById('amountUsers').textContent = "Users connected : " + message.amount;
+  document.getElementById('serverMessages').textContent = "" + message;
+  
+})
+
+// Get the pseudo of the client from the server.
+socket.on('newUser', function(userName) {
+  console.log(userName);
+})
+
+// LogOn && logOff refresh list of user, and amount of user, display a message who said an user has been connected or disconnected to everyone
+
+socket.on('logOn', function(count) {
+  
+  let info = document.createElement("p");
+  
+  info.setAttribute("class", "infoOn");
+  
+  info.textContent = count.content + " joined the party !";
+  
+  document.getElementById("chat").prepend(info)
+  
+  document.getElementById('amountUsers').textContent = "Users connected : " + count.amount;
+  
+  let result = ""
+
+  for (let i = 0; i < count.users.length; i++) {
+    
+      result = result + "<br>"+ count.users[i] + "<br>";
+    
+  }
+
+  document.getElementById("usersList").innerHTML = "<p id='amountUsers'> Users connected : " + count.amount + "</p>" + result;
+
+
+})
+
+socket.on('logOff', function(userName) {
+
+  let info = document.createElement("p");
+  
+  info.setAttribute("class", "infoOff");
+  
+  info.textContent = userName.content + " Has been disconnected.";
+
+  document.getElementById("chat").prepend(info)
+  
+  document.getElementById('amountUsers').textContent = "Users connected : " + userName.amount;
+  
+  let result = ""
+
+  for (let i = 0; i < userName.users.length; i++) {
+    
+      result = result + "<br>"+ userName.users[i] + "<br>";
+    
+  }
+
+  document.getElementById("usersList").innerHTML = "<p id='amountUsers'> Users connected : " + userName.amount + "</p>" + result;
 
 })
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+
 'use strict';
 
 const express = require('express');
 const socketIO = require('socket.io');
-const path = require('path');
-process.env.PWD = process.cwd();
+const ent = require('ent');
 
+const path = require('path');
+
+process.env.PWD = process.cwd();
 const PORT = process.env.PORT || 3000;
 const server = express()
 
@@ -20,23 +23,101 @@ const server = express()
 
 const io = socketIO(server);
 
+
+
 let amountUser = 0;
 
-let hello = "Hello, welcome on our chat service have a good talking !"
+let hello = "Hello, welcome on our chat service have a good talking !";
+
+// Server provide a random name :
+
+let nameList = ["Strawberry","Pineapple","Pink","Tiger","Wolf","Hero","Legend","Otter","Kitten"];
+
+let nameStuffList = ["Angry","Anxious","Curious","Sleeping","Incredible","Tiny","Big","Invisible"];
+
+let users = [];
+
+function getRandomInt(max) {
+
+  return Math.floor(Math.random() * Math.floor(max));
+
+}
+
 
 io.on('connection', (socket) => {
   
+  // Provide random userName, and push it into an array.
+
+  let userName = nameStuffList[getRandomInt(nameStuffList.length)]
+
+  userName = userName + nameList[getRandomInt(nameList.length)]
+
+  userName = userName + String(getRandomInt(999))
+
+ // Ensure we can't get two equals userName. ( Even if the probability is really weak ! )
+  for (let i = 0; i < users.length; i++) {
+    
+    if(users[i] === userName)
+      {
+        userName = nameStuffList[getRandomInt(nameStuffList.length)]
+
+        userName = userName + nameList[getRandomInt(nameList.length)]
+
+        userName = userName + String(getRandomInt(999))
+      }
+  }
+
+  socket.pseudo = userName;
+
+  users.push(userName)
+
+  hello = "Hello, your logged in as : " + userName + " welcome on our chat service have a good talking !";
+
+ 
+
+  // Increment users amount 
+  
   amountUser++;
+  
   amountUser = String(amountUser)
 
-  io.emit("hello",{ content: hello, amount: amountUser })
+  // Emit data with user name add new amountUser value to EVERYONE.
+  io.emit("logOn",{content: userName, amount: amountUser, users: users })
 
+  // Greetings to arrival for the client
+  socket.emit("hello",hello)
+
+  
+  
+  // Emit for the client an event newUser
+  socket.emit("newUser",(socket.pseudo))
+
+  // On client disconnection
   socket.on('disconnect', () => {
   
+
+
+  // Running the array and remove is name. 
+  
+  for( var i = 0; i < users.length; i++){ 
+   
+    if ( users[i] === socket.pseudo) {
+   
+      users.splice(i, 1); 
+   
+    }
+   }
+
+  // Decrement user
   amountUser--;
-  io.emit("hello",{ content: hello, amount: amountUser })
+
+  // Emit data with user name add new amountUser value to EVERYONE.
+  io.emit("logOff",{ content: socket.pseudo, amount: amountUser, users: users})
+
+  
   
   });
+
 
 });
 
